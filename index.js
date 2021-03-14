3d76ce15f82fecde05fdb41c9854dca429a114b9
ASHISH PAL <68744952+ASHISHPAL1999@users.noreply.github.com>
2021-03-14 22:05:05 +0530

    route added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express =require("express")
 const app=express()
+app.get('/',(req,res)=>{
+    res.send(`welcome alien!`)
+
+})
 app.listen(2000,(req,res)=>{
     console.log("port running at 2000")
 })
